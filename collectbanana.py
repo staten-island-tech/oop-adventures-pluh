bd1813005744d9dafa156b498cda4db952e32c3b
Eddiel321 <EDdiel32@nycstudents.net>
2025-01-06 10:09:00 -0500

    Create collectbanana.py

---
+++ collectbanana.py
@@ -0,0 +1,15 @@
+import os
+import turtle
+import time 
+import random
+
+score  = 0
+high_score = 0
+delay = 0.1
+
+
+window1 = turtle.Screen()
+window1.title("banana game")
+window.setup(width = 600, height = 600)
+window1.tracer(0)
+time.sleep(10)
